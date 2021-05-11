a7e655003c59b3e8e6fd082d5c627fc97350bce6
DavidAlen123 <83995118+DavidAlen123@users.noreply.github.com>
2021-05-11 07:54:01 +0530

    Radius of a circle

---
+++ Radius of a circle.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 11 07:15:17 2021
+
+@author: DAVID ALEN
+"""
+
+from math import pi
+r = float(input ("Input the radius of the circle : "))
+print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
+
